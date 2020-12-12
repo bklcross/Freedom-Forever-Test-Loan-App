2d12665d582d03102a2c99c5886cc142d47edc02
Brian Lee <brian@fusionmedia.com>
2020-12-12 10:22:16 -0800

    set up react app

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Requiring our routes
-app.use(routes);
+//requiring our routes
+//app.use(routes);
 
-// Syncing our database and logging a message to the user upon success
+//syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
