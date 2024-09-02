f1cef54a07e8cbaa82dcca0491f0dbbbbcf7479f
bklcross <bklcross@gmail.com>
2024-09-01 19:49:50 -0700

    update client

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import Welcome from "./components/Welcome";
 import LoanForm from "./components/LoanForm";
 import LoanApps from "./components/LoanApps";
 
-
 function App() {
   return (
     <Router>
       <div className="app">
         <Nav />
-        <Switch>
-          <Route exact path="/" component={Welcome} />
-          <Route exact path="/loan-form" component={LoanForm} />
-          <Route exact path="/loan-apps" component={LoanApps} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/loan-form" element={<LoanForm />} />
+          <Route path="/loan-apps" element={<LoanApps />} />
+        </Routes>
         <Footer />
       </div>
-  </Router>
+    </Router>
   );
 }
 
